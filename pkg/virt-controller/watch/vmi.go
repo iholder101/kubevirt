be453d8e33964638b721f833f592ec37cd46898c
Jed Lejosne <jed@redhat.com>
2023-03-21 16:56:05 -0400

    added e2e test suite for CPU hotplug
    Signed-off-by: Jed Lejosne <jed@redhat.com>
    Co-authored-by: enp0s3 <ibezukh@redhat.com>
    Co-authored-by: Vladik Romanovsky <vromanso@redhat.com>

--- pkg/virt-controller/watch/vmi.go
+++ pkg/virt-controller/watch/vmi.go
@@ -1234,7 +1234,6 @@ func (c *VMIController) sync(vmi *virtv1.VirtualMachineInstance, pod *k8sv1.Pod,
 	}
 
 	if !isTempPod(pod) && isPodReady(pod) {
-
 		if vmispec.SRIOVInterfaceExist(vmi.Spec.Domain.Devices.Interfaces) {
 			networkPCIMapAnnotationValue := sriov.CreateNetworkPCIAnnotationValue(
 				vmi.Spec.Networks, vmi.Spec.Domain.Devices.Interfaces, pod.Annotations[networkv1.NetworkStatusAnnot],
